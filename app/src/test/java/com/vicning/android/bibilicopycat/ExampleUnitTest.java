d47dfdd787a92c77a87d1d975eef9068ae3ba970
Neil <nealoveyou@gmail.com>
2016-08-21 16:35:21 +0800

    Code clean up and some adjustments.

--- app/src/test/java/com/vicning/android/bibilicopycat/ExampleUnitTest.java
+++ app/src/test/java/com/vicning/android/bibilicopycat/ExampleUnitTest.java
@@ -1,15 +1,6 @@
 package com.vicning.android.bibilicopycat;
 
-import com.vicning.android.bibilicopycat.common.Secret;
-import com.vicning.android.bibilicopycat.utils.MD5Util;
-
 import org.junit.Test;
-
-import java.io.UnsupportedEncodingException;
-import java.net.URLEncoder;
-import java.security.MessageDigest;
-import java.util.LinkedHashMap;
-
 import static org.junit.Assert.*;
 
 /**
